88418421384fc171c7c48f1764b4dcca52e4df43
GoMimiGO <waltetta@gmail.com>
2023-03-21 00:57:56 -0400

    Delete Utils.js

--- Utils.js
+++
@@ -1,8 +0,0 @@
-function showMessage(message)
-    {document.getElementById('message').textContent = message;    }
-function myFunction() 
-    {document.getElementById("demo1").innerHTML="Infant Notes"}
-function myFunction() 
-    {document.getElementById("demo2").innerHTML="Toddler Notes"}
-function myFunction() 
-    {document.getElementById("demo3").innerHTML="Preschool Notes"}
